c8aabf6b2d12bd6c6451fc3b177f1926c1cbd961
pooyajjj <pooya.jv@gmail.com>
2022-12-28 14:45:24 +0330

    kavenegar panel sms has been fixed

--- utils.py
+++ utils.py
@@ -16,3 +16,8 @@ def send_otp_code(phone_number, code):
         print(e)
     except HTTPException as e: 
         print(e)
+
+
+# api = KavenegarAPI('464A41356F656344324951476B42494B594C6E6A6A666A6A4E63456C3441452F3344317479356575614B673D')
+# params = { 'sender' : '', 'receptor': '09212368201', 'message' :'.وب سرویس پیام کوتاه کاوه نگار' }
+# response = api.sms_send( params)

ccae40298479ce65aedb0a5e2c71459c20a8f67a
pooyajjj <pooya.jv@gmail.com>
2023-01-03 00:49:49 +0330

    bucket.py & bucket.html has been created

--- Store/urls.py
+++ Store/urls.py
@@ -21,6 +21,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace='home')),
-    path('api/', include('home.urls', namespace='home')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

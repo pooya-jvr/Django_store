4155bd3e8aa28bcd69feb7fc4741019ba7fa342c
pooyajjj <pooya.jv@gmail.com>
2023-01-02 22:56:11 +0330

    celery conf has been created

--- Store/urls.py
+++ Store/urls.py
@@ -21,5 +21,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace='home')),
+    path('api/', include('home.urls', namespace='home')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

68a4942b14d59b983ba13e0451e16dbc47a18964
pooyajjj <pooya.jv@gmail.com>
2023-01-14 03:11:29 +0330

    bucket delete issue has been fixed

--- home/urls.py
+++ home/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name = 'home'),
     path('bucket/', views.BucketHome.as_view(), name = 'bucket'),
     path('delete_bucket/<key>', views.DeleteBucketObjects.as_view(), name = 'delete_obj_bucket'),
-    path('download_bucket/<key>', views.DownloadBucketObject.as_view(), name='delete_obj_bucket'),
+    path('download_bucket/<key>', views.DownloadBucketObject.as_view(), name='download_obj_bucket'),
     path('upload_bucket/', views.UploadBucketObject.as_view(), name = 'upload_bucket'),
     path('<slug:slug>/', views.ProductDetailView.as_view(), name = 'product_detail'),
 ]

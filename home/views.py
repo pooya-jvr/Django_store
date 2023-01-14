737cc1ad18ce9977c86a7a3342edac0d3a791512
pooyajjj <pooya.jv@gmail.com>
2023-01-14 23:59:55 +0330

    category models and dropdown has been created

--- home/views.py
+++ home/views.py
@@ -14,7 +14,7 @@ class HomeView(View):
 
     def get(self, request, category_slug=None):
         products = Product.objects.filter(available=True)
-        categories = Category.objects.all()
+        categories = Category.objects.filter(is_sub=False)
         if category_slug:
             category = Category.objects.get(slug=category_slug)
             products = products.filter(category=category)

9a01591bd3c3c7236a265c8d5f9711edb92589e6
pooyajjj <pooya.jv@gmail.com>
2023-01-22 19:52:12 +0330

    cart add handeled

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-
+from .cart import Cart
+from home.models import Product
+from .forms import CardAddForm
 
 class CartView(View):
 
@@ -10,4 +12,10 @@ class CartView(View):
 class CardAddView(View):
     
     def post(self, request, product_id):
-        pass
+        cart = Cart(request)
+        product = get_object_or_404(Product, id = product_id)
+        form = CardAddForm(request.POST)
+        if form.is_valid():
+            cart.add(product, form.cleaned_data['quantity'])
+        return redirect('orders:cart')
+        

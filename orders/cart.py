eb1ffb3135f0945a54342df6baf6b7c5ee3084f8
pooya-dev <pooya.jv@gmail.com>
2023-02-21 15:53:32 +0330

    cart total price has been created

--- orders/cart.py
+++ orders/cart.py
@@ -33,3 +33,7 @@ class Cart:
         
     def save(self):    
         self.session.modified = True
+
+
+    def get_total_price(self):
+        return sum(int(item['price']) * item['quantity'] for item in self.cart.values())

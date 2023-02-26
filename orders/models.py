a67e88d5c45bfc8a71c1ddc0ac54d1ac2b0ddd6e
pooya-dev <pooya.jv@gmail.com>
2023-02-27 01:36:59 +0330

    order detail view has been created

--- orders/models.py
+++ orders/models.py
@@ -27,7 +27,7 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.id
+        return f'{self.id}'
 
     def get_cost(self):
         return self.price * self.quantity

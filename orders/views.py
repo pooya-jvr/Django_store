1f630a7c8a14c1eaa279e6340dfa2b9a6107fdff
pooya-dev <pooya.jv@gmail.com>
2023-03-02 02:05:46 +0330

    order pay view has been created

--- orders/views.py
+++ orders/views.py
@@ -5,6 +5,12 @@ from home.models import Product
 from .forms import CardAddForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Order, OrderItem
+import requests
+from django.conf import settings
+import json
+from django.http import HttpResponse
+
+
 class CartView(View):
 
     def get(self, request):
@@ -49,3 +55,41 @@ class OrderCreateView(LoginRequiredMixin, View):
         return redirect('orders:order_detail', order.id)
 
 
+if settings.SANDBOX:
+    sandbox = 'sandbox'
+else:
+    sandbox = 'www'
+
+\
+
+ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest.json"
+ZP_API_VERIFY = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
+ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
+
+description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
+# Optional
+# Important: need to edit for realy server.
+CallbackURL = 'http://127.0.0.1:8080/orders/verify/'
+Amount = Order.get_total_price
+class OrderPayView(LoginRequiredMixin, View):
+    def get(self, request, order_id):
+        order = Order.objects.get(id=order_id)
+        Phone = request.user.phone_number
+        data = {
+            "MerchantID": settings.MERCHANT,
+            "Amount": Amount,
+            "Description": description,
+            "Phone": Phone,
+            "CallbackURL": CallbackURL,
+        }
+        header = {"accept": "application/json", "content-type": "application/json'"}
+
+        data = requests.post(url=ZP_API_REQUEST, data=json.dumps(data), headers=header)
+            
+        authority = data.json()['data']['authority']
+        if len(data.json()['errors']) == 0:
+                return redirect(ZP_API_STARTPAY.format(authority=authority))
+        else:
+                e_code = data.json()['errors']['code']
+                e_message = data.json()['errors']['message']
+                return HttpResponse(f"Error code: {e_code}, Error Message: {e_message}")

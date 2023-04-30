38083f235d2c7ca5b6c3545482995945abd889f0
pooya <pooya.jv@gmail.com>
2023-04-30 16:46:50 +0330

    test

--- orders/apps.py
+++ orders/apps.py
@@ -5,5 +5,3 @@ class OrdersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "orders"
 
-
-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
